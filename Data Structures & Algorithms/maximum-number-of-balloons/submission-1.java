65fced521214c55d9a1e50bcf7dd4ac059ac7642
Shahbaz Akhtar <shahbazakhtarmg@gmail.com>
2026-07-12 17:24:10 +0530

    Add: maximum-number-of-balloons - submission-1

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        int[] freq = new int[26];
+
+        for (int i = 0; i < text.length(); i++) {
+            freq[text.charAt(i) - 'a']++;
+        }
+
+        return Math.min(
+                Math.min(freq['b' - 'a'], freq['a' - 'a']),
+                Math.min(
+                        Math.min(freq['l' - 'a'] / 2, freq['o' - 'a'] / 2),
+                        freq['n' - 'a']
+                )
+        );
+    }
+}
