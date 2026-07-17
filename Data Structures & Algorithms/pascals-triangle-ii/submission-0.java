3ec66f32eb175d4f990e6c0877a7c30358322f36
Shahbaz Akhtar <shahbazakhtarmg@gmail.com>
2026-07-17 20:27:11 +0530

    Add: pascals-triangle-ii - submission-0

---
+++ Data Structures & Algorithms/pascals-triangle-ii/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 1; i <= numRows; i++) {
+            result.add(new ArrayList<>(Collections.nCopies(i, 1)));
+        }
+
+        for(int i = 2; i < result.size(); i++) {
+            List<Integer> prev = result.get(i-1);
+            List<Integer> curr = result.get(i);
+            for(int j=1; j<curr.size()-1; j++) {
+                curr.set(j, prev.get(j-1) + prev.get(j));
+            }
+        }
+
+        return  result;
+    }
+
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> result = generate(rowIndex+1);
+
+        return result.get(rowIndex);
+    }
+}
