552d072c81fc98a9d7d68fef425925646c2141c8
Shahbaz Akhtar <shahbazakhtarmg@gmail.com>
2026-09-13 13:41:00 +0530

    Add: number-of-subsequences-that-satisfy-the-given-sum-condition - submission-0

---
+++ Data Structures & Algorithms/number-of-subsequences-that-satisfy-the-given-sum-condition/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numSubseq(int[] nums, int target) {
+        final int MOD = 1_000_000_007;
+        int n = nums.length;
+        int[] powersOfTwo = new int[n];
+        powersOfTwo[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            powersOfTwo[i] = (powersOfTwo[i - 1] * 2) % MOD;
+        }
+
+        Arrays.sort(nums);
+
+        int left = 0;
+        int right = n - 1;
+        int count = 0;
+
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                count = (count + powersOfTwo[right - left]) % MOD;
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return count;
+    }
+}
